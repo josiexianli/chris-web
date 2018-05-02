c92f9f6f8627f23d86303db5de7f96398702b224
Yuan Yao <yuanyao@Yuans-MacBook-Pro-7.local>
2018-05-01 19:54:54 -0700

    SAVE FOR ADD JUMPER

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+app.get('*', (req, res, next) => {
+    if (req.headers["x-forwarded-proto"] === "https") {
+        return next();
+    }
+    res.redirect("https://" + req.headers.host + req.url);
+});
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
